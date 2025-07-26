630aaed1a15cedcc4a1446c8181c27af4f6408c1
Zack Shone <zackariahshone.ta@gmail.com>
2025-07-26 18:46:47 -0400

    sdaf

--- backend/dbconnection/connection.js
+++ backend/dbconnection/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
-const uri = "mongodb+srv://zackariahshone:Zs72756AR4010!@nonprofitcluster.oe6muis.mongodb.net/?retryWrites=true&w=majority&appName=nonProfitCluster";
+// const uri = "mongodb+srv://zackariahshone:Zs72756AR4010!@nonprofitcluster.oe6muis.mongodb.net/?retryWrites=true&w=majority&appName=nonProfitCluster";
+const uri = "mongodb+srv://zackariahshone:Zs72756AR4010!@timeclock.dmzpirt.mongodb.net/"
 
 const db = 'mongodb://localhost:27017/ercDB';
 const connection = mongoose.connect(uri);
